d4e11199fc77f62ec3da67b243255aa422b19d54
Camilo Mercado <j.mercado@uniandes.edu.co>
2025-04-23 20:41:19 -0500

    Refactor main.dart: reorganizar imports, mejorar manejo de conectividad y actualizar lógica de recarga de la aplicación

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,17 +1,20 @@
+import 'dart:async';
 import 'package:ecosphere/firebase_options.dart';
 import 'package:ecosphere/routes/routes.dart';
 import 'package:ecosphere/services/connectivity.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
-import 'package:cloudinary_url_gen/cloudinary.dart';
 import 'package:cloudinary_flutter/cloudinary_context.dart';
+import 'package:cloudinary_url_gen/cloudinary.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'dart:async';
+
+// Referencia global para forzar reload
+late _MyAppState myAppStateRef;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,10 +31,7 @@ Future<void> main() async {
       print("Firebase inicializado correctamente");
     }
 
-    // Probar conexión con Firebase Auth
     await testFirebaseAuthConnection();
-
-    // Solicitar permisos de ubicación
     await requestLocationPermission();
   } catch (e) {
     if (kDebugMode) {
@@ -40,10 +40,6 @@ Future<void> main() async {
   }
 
   await dotenv.load(fileName: '.env');
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
   FlutterError.onError = (details) {
     FirebaseCrashlytics.instance.recordFlutterError(details);
@@ -55,22 +51,23 @@ Future<void> main() async {
   };
 
   CloudinaryContext.cloudinary = Cloudinary.fromCloudName(
-      cloudName: 'dhrkcqd33', apiKey: '537811732293891');
+    cloudName: 'dhrkcqd33',
+    apiKey: '537811732293891',
+  );
 
-  // Obtener la API key de ChatGPT
   final chatGptApiKey = dotenv.env['KEY_ECOSPHERE'] ?? '';
 
-  // Escuchar el Stream de conectividad
   connectivityStream().listen((isConnected) {
     if (!isConnected) {
       showConnectivityToast();
+    } else {
+      myAppStateRef.reloadApp();
     }
   });
 
   runApp(MyApp(chatGptApiKey: chatGptApiKey));
 }
 
-/// ✅ **Función para probar conexión con Firebase Authentication**
 Future<void> testFirebaseAuthConnection() async {
   try {
     await FirebaseAuth.instance.signInAnonymously();
@@ -84,7 +81,6 @@ Future<void> testFirebaseAuthConnection() async {
   }
 }
 
-/// **Función para solicitar permisos de ubicación**
 Future<void> requestLocationPermission() async {
   LocationPermission permission = await Geolocator.checkPermission();
   if (permission == LocationPermission.denied) {
@@ -101,35 +97,54 @@ Future<void> requestLocationPermission() async {
   }
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final String chatGptApiKey;
 
   const MyApp({super.key, required this.chatGptApiKey});
 
   @override
+  State<MyApp> createState() {
+    myAppStateRef = _MyAppState();
+    return myAppStateRef;
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  Key _rebuildKey = UniqueKey();
+
+  void reloadApp() {
+    setState(() {
+      _rebuildKey = UniqueKey(); // Cambiar key para forzar rebuild
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Recycling App',
-      theme: ThemeData(
-        primarySwatch: MaterialColor(
-          0xFF49447E,
-          <int, Color>{
-            50: Color(0xFFE8E7F0),
-            100: Color(0xFFC5C3DA),
-            200: Color(0xFF9E9BC1),
-            300: Color(0xFF7773A8),
-            400: Color(0xFF59558F),
-            500: Color(0xFF49447E),
-            600: Color(0xFF413C76),
-            700: Color(0xFF38326B),
-            800: Color(0xFF302961),
-            900: Color(0xFF211A4E),
-          },
+    return KeyedSubtree(
+      key: _rebuildKey,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Recycling App',
+        theme: ThemeData(
+          primarySwatch: MaterialColor(
+            0xFF49447E,
+            <int, Color>{
+              50: Color(0xFFE8E7F0),
+              100: Color(0xFFC5C3DA),
+              200: Color(0xFF9E9BC1),
+              300: Color(0xFF7773A8),
+              400: Color(0xFF59558F),
+              500: Color(0xFF49447E),
+              600: Color(0xFF413C76),
+              700: Color(0xFF38326B),
+              800: Color(0xFF302961),
+              900: Color(0xFF211A4E),
+            },
+          ),
         ),
+        initialRoute: '/',
+        routes: Routes.routes,
       ),
-      initialRoute: '/',
-      routes: Routes.routes,
     );
   }
 }
